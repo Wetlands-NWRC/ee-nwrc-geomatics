92633d7d1e43b5d0aff9fbec3d1e5b230f869042
Ryan Hamilton <ryangilberthamilton@gmail.com>
2023-07-20 12:12:38 -0400

    added a data cube dataset and props

--- src/rftbx/datasets.py
+++ src/rftbx/datasets.py
@@ -19,3 +19,22 @@ class ALOS2(ee.ImageCollection):
 class NASADEM(ee.ImageCollection):
     def __init__(self):
         super().__init__("NASA/NASADEM_HGT/001")
+
+
+class DataCube(ee.ImageCollection):
+    SEASON_PREFIX = {"spring": "a_spri_b", "summer": "b_summ_b", "fall": "c_fall_b"}
+
+    def __init__(self, args):
+        super().__init__(args)
+
+    @property
+    def spring(self):
+        return self.select(f'{self.SEASON_PREFIX["spring"]}.*')
+
+    @property
+    def summer(self):
+        return self.select(f'{self.SEASON_PREFIX["summer"]}.*')
+
+    @property
+    def fall(self):
+        return self.select(f'{self.SEASON_PREFIX["fall"]}.*')
