d54845487a122ac304256c0e943a3dd557acd694
Ryan Hamilton <ryangilberthamilton@gmail.com>
2023-07-21 14:43:35 -0400

    added Random forest pipeline interface

---
+++ src/rftbx/pipeline.py
@@ -0,0 +1,27 @@
+import abc
+from abc import ABCMeta, abstractmethod
+
+
+class IRandomForestPipeline(metaclass=ABCMeta):
+
+    @classmethod
+    def __subclasshook__(cls, subclass):
+        return (hasattr(subclass, 'preprocessing') and
+                callable(subclass.preprocessing) and
+                hasattr(subclass, 'processing') and
+                callable(subclass.processing) and
+                hasattr(subclass, 'postprocessing') and
+                callable(subclass.postprocessing))
+
+    @abstractmethod
+    def preprocessing(self):
+        raise NotImplementedError
+
+    @abstractmethod
+    def processing(self):
+        raise NotImplementedError
+
+    @abstractmethod
+    def postprocessing(self):
+        raise NotImplementedError
+
