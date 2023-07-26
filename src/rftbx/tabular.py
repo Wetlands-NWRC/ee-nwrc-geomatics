7c548a41e912bd2c4ddaebef034c7c21bde2984a
Ryan Hamilton <ryangilberthamilton@gmail.com>
2023-07-26 14:17:58 -0400

    Training Data is working as expected

--- src/rftbx/tabular.py
+++ src/rftbx/tabular.py
@@ -7,50 +7,50 @@ ColumnMapping = Union[Dict[str, int], ee.Dictionary]
 Lookup = ee.Dictionary
 
 
-class TabularFunctions:
-    def remap(
-        collection: ee.FeatureCollection, column: ColumnName, mapping: ColumnMapping
-    ) -> ee.FeatureCollection:
-        if isinstance(mapping, dict):
-            mapping = ee.Dictionary(mapping)
+class TrainingData(ee.FeatureCollection):
+    def __init__(self, args, opt_column=None):
+        super().__init__(args, opt_column)
+        self.class_property: ColumnName = 'Wetland'
 
-        lookup_in = ee.List(mapping.keys())
-        lookup_out = ee.List(mapping.values())
-        return collection.remap(lookup_in, lookup_out, column)
+        # TODO add a validation check to ensure the class property is in the feature collection
 
-    def insert_xy(x: str = None, y: str = None) -> callable:
-        x = "x" if x is None else x
-        y = "y" if y is None else y
-
-        def insert_xy_inner(feature: ee.Feature) -> ee.Feature:
-            geom = feature.geometry().coordinates()
-            return feature.set(
-                {
-                    x: geom.get(0),
-                    y: geom.get(1),
-                }
-            )
-
-        return insert_xy_inner
-
-    def lookup(collection: ee.FeatureCollection, on: ColumnName) -> Lookup:
-        keys = collection.aggregate_array(on).distinct().sort()
+    @property
+    def lookup(self) -> Lookup:
+        keys = self.aggregate_array(self.class_property).distinct().sort()
         values = ee.List.sequence(1, keys.size())
         return ee.Dictionary.fromLists(keys, values)
 
-    def lookup_2_feature_collection(lookup: Lookup):
-        keys = lookup.keys()
-        values = lookup.values()
-        return ee.FeatureCollection(
-            keys.zip(values).map(
-                lambda x: ee.Feature(None, {"key": x.get(0), "value": x.get(1)})
-            )
-        )
 
-@dataclass
-class TrainingData:
-    col: ee.FeatureCollection
-    lookup: Lookup = None
+    def remap_(self, mapping: ColumnMapping):
+        if isinstance(mapping, dict):
+            # if any key value is not a string then convert to string
+            for key in mapping.keys():
+                if not isinstance(key, str):
+                    mapping[str(key)] = mapping.pop(key)
+            mapping = ee.Dictionary(mapping)
+
+        lookup_in = ee.List(mapping.keys())
+        lookup_out = ee.List(mapping.values()).map(lambda x: ee.Number(x).int())
+
+        return TrainingData(self.remap(lookup_in, lookup_out, self.class_property))
 
 
+    def add_x_col(self, x: ColumnName = None):
+        """ adds the x coordinate column to the feature collection
+        Args:
+            x (ColumnName, optional): [description]. Defaults to x.
+
+        """
+        x = 'x' if x is None else x
+        def _add_x_col(feature: ee.Feature) -> ee.Feature:
+            geom = feature.geometry().coordinates()
+            return feature.set({x: geom.get(0)})
+        return self.map(_add_x_col)
+
+    def add_y_col(self, y: ColumnName = None):
+        y = 'y' if y is None else y
+        def _add_y_col(feature: ee.Feature) -> ee.Feature:
+            geom = feature.geometry().coordinates()
+            return feature.set({y: geom.get(1)})
+        return self.map(_add_y_col)
 
