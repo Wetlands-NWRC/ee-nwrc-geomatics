96b3f417d482ebfa2def8a3b88ac4a440ece64db
Ryan Hamilton <ryangilberthamilton@gmail.com>
2023-08-01 09:27:54 -0400

    this test is failing not sure if matirx is being construct

---
+++ tests/test_stats.py
@@ -0,0 +1,37 @@
+import unittest
+from pprint import pprint
+import ee
+
+from rftbx.stats import Assessment
+
+ee.Initialize()
+
+class TestAssessmentBuilder(unittest.TestCase):
+    def setUp(self) -> None:
+        array = ee.Array([
+            [1,2,3],
+            [4,5,6],
+            [7,8,9]
+        ])
+
+        self.matrix = ee.ConfusionMatrix(array)
+
+    
+    def test_assessment_builder(self):
+        asmnt = Assessment(self.matrix)
+
+        try:
+            pprint(self.matrix.getInfo())
+            (asmnt
+             .add_matrix()
+             .add_accuracy()
+             .add_producers()
+             .add_consumers()
+             .add_labels(['a', 'b', 'c', 'd', 'e', 'f'])
+             .build()
+            )
+
+            pprint(asmnt.assessment.getInfo())
+        except ee.EEException as e:
+            print(e)
+            self.fail("Assessment builder failed")
