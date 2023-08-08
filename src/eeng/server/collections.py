00a330b5e3fda34c81c024dc9be15f867bda9967
Ryan Hamilton <ryangilberthamilton@gmail.com>
2023-08-08 13:41:12 -0400

    moved creator classes

--- src/eeng/server/collections.py
+++ src/eeng/server/collections.py
@@ -1,80 +1,30 @@
-import ee
-from ee import Geometry
+from dataclasses import dataclass, field
 
-from math import pi
-from .calc import NDVI
+import ee
+
+
+@dataclass(frozen=True)
+class ImageCollectionIDs:
+    """ Contains the image collection ids for the different data sources """
+    s1 = field(default="COPERNICUS/S1_GRD")
+    s2 = field(default="COPERNICUS/S2")
+    s2_sr = field(default="COPERNICUS/S2_SR")
+    s2_cloud_prob = field(default="COPERNICUS/S2_CLOUD_PROBABILITY")
+    alos = field(default="JAXA/ALOS/PALSAR/YEARLY/SAR")
 
 
 class ImageCollectionCreator:
-    def __init__(self, collection_id: str) -> None:
+    def __init__(self, collection_id: str, start, end, aoi) -> None:
         self.collection_id = collection_id
+        self.start = start
+        self.end = end
+        self.aoi = aoi
 
-    def create_collection(self, start, end, aoi) -> ee.ImageCollection:
-        return ee.ImageCollection(self.collection_id).filterDate(start, end).filterBounds(aoi)
-    
-    @classmethod
-    def s1_collection_factory(cls):
-        return cls("COPERNICUS/S1_GRD")
-
-    @classmethod
-    def s2_sr_collection_factory(cls):
-        return cls("COPERNICUS/S2_SR")
-
-    @classmethod
-    def s2_cloud_probability_collection_factory(cls):
-        return cls("COPERNICUS/S2_CLOUD_PROBABILITY")
-    
-    @classmethod
-    def s2_toa_collection_factory(cls):
-        return cls("COPERNICUS/S2")
-    
-    @classmethod
-    def alos_collection_factory(cls):
-        return cls("JAXA/ALOS/PALSAR/YEARLY/SAR")
-
-
-class Sentinel1Creator(ImageCollectionCreator):
-    def get_s1_dv(self, start, end, aoi) -> ee.ImageCollection:
-        polerizations = ee.Filter([
-            ee.Filter.listContains('transmitterReceiverPolarisation', 'VV'),
-            ee.Filter.listContains('transmitterReceiverPolarisation', 'VH')
-        ])
-        return self.s1_collection_factory().create_collection(start=start, end=end, aoi=aoi).filter(polerizations).select('V.*')
-    
-    def get_s1_dh(self, start, end, aoi) -> ee.ImageCollection:
-        return NotImplemented
-
-
-class Sentinel2(ImageCollectionCreator):
-    def top_of_atmosphere(self):
-        return NotImplemented
-    
-    def get_s2_sr(self, start, end, aoi) -> ee.ImageCollection:
-        """ get Sentinel2 Surface Reflectance image collection """
-        return self.s2_sr_collection_factory().create_collection(start, end, aoi)
-
-    def get_s2_cloud_propability(self, start, end, aoi):
-        return self.s2_cloud_probability_collection_factory().create_collection(start, end, aoi)
-    
-    def s2Cloudless(self, start, end, aoi, cloud_px_percent: int = 60) -> ee.ImageCollection:
-        prob = self.get_s2_cloud_propability(start, end, aoi)
-        s2 = self.get_s2_cloud_propability(start, end, aoi).filter(ee.Filter.lt("CLOUDY_PIXEL_PERCENTAGE", cloud_px_percent))
-
-        join = ee.Join.saveFirst('s2cloudless').apply(**{
-            'primary': s2,
-            'secondary': prob,
-            'condition': ee.Filter.equals(**{
-                'leftField': 'system:index',
-                'rightField': 'system:index'
-            })
-        })
-
-        return ee.ImageCollection(join)
-
-
-class TimeSeriesCreator(ImageCollectionCreator):
-    def get_time_series(self, start, end, aoi, omega: float = 1.0):
-        return TimeSeries(self.create_collection(start, end, aoi)).addTime(omega).addConstant()
+    def get_collection(self, filter_function: ee.Filter) -> ee.ImageCollection:
+        date_filter = ee.Filter.date(self.start, self.end)
+        combo_filter = ee.Filter.And(date_filter, filter_function)
+        collection = ee.ImageCollection(self.collection_id).filterBounds(self.aoi).filter(combo_filter)
+        return collection
 
 
 class DataCube(ee.ImageCollection):
