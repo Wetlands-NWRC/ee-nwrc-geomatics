60cedd0d739c49ae77bd738ce2119c83f614ed9f
Ryan Hamilton <ryangilberthamilton@gmail.com>
2023-08-08 13:42:07 -0400

    added bounding class methods for the factories in cfuncs

--- src/eeng/__init__.py
+++ src/eeng/__init__.py
@@ -7,6 +7,13 @@ ee.ImageCollection.addDependent = addDependent
 ee.ImageCollection.addConstant = addConstant
 ee.ImageCollection.addTime = addTime
 ee.ImageCollection.addHarmonics = addHarmonics
+ee.ImageCollection.addCloudMask = add_cloud_mask
+ee.ImageCollection.sentinel2SR = sentinel2SR
+ee.ImageCollection.sentinel2TOA = sentinel2TOA
+ee.ImageCollection.sentinel2CloudProbability = sentinel2CloudProbability
+ee.ImageCollection.sentinel1DV = sentinel1DV
+ee.ImageCollection.alos = alos
+ee.ImageCollection.sentinel2Cloudless = sentinel2Cloudless
 
 # bound functions to the ee.FeatureCollection class
 ee.FeatureCollection.addXCordinate = add_x_coordinate
