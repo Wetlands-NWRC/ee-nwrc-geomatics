8665e13a2ffad9224327cde0f4539c6528aae921
Ryan Hamilton <ryangilberthamilton@gmail.com>
2023-08-18 10:52:03 -0400

    updated to be a builder

--- src/eeng/server/tables.py
+++ src/eeng/server/tables.py
@@ -3,48 +3,33 @@ import ee
 ColumnName = str
 
 
-class TrainingPoints(ee.FeatureCollection):
+class TrainingPoints:
+    """This is a builder Class that is used to build Traning Points dataset from a Feature Collection that is assuemd to be a collection of points"""
+
     def __init__(
-        self, args, classProperty: ColumnName = None, opt_column: ColumnName = None
+        self,
+        points: ee.FeatureCollection,
+        classProperty: ColumnName = None,
     ):
-        super().__init__(args, opt_column)
+        self.properties = []
+        self.points = points
         self.classProperty = classProperty
-        self._properties = []
-        self.labels = classProperty
-        self.values = self.labels
+        self._add_propertity(classProperty)
 
     @property
-    def properties(self):
-        return self._properties
+    def labels(self):
+        return self.points.aggregate_array(self.classProperty).distinct()
+
+    @property
+    def values(self):
+        return ee.List.sequence(1, self.labels.size())
 
     @property
     def lookup(self) -> ee.Dictionary:
         return ee.Dictionary.fromLists(self.labels, self.values)
 
-    @property
-    def classProperty(self):
-        return self._classProperty
-
-    @classProperty.setter
-    def classProperty(self, value):
-        self.classProperty = "class_name" if value is None else value
-        self._properties.append(self.classProperty)
-
-    @property
-    def labels(self):
-        return self._labels
-
-    @labels.setter
-    def labels(self, value):
-        self._labels = self.aggregate_array(value).distinct()
-
-    @property
-    def values(self):
-        return self._values
-
-    @values.setter
-    def values(self, value: ee.List):
-        self._values = ee.List.sequence(1, value.size())
+    def __repr__(self) -> str:
+        return f"TrainingPoints({self.points}, {self.classProperty})"
 
     def addXCoordinate(self):
         """adds the x coordinate of the geometry to the feature collection"""
@@ -53,8 +38,9 @@ class TrainingPoints(ee.FeatureCollection):
             geom = feature.geometry().centroid(1).coordinates().get(0)
             return feature.set("x", geom)
 
-        self._properties.append("x")
-        return self.map(add_x_coordinate)
+        self._add_propertity("x")
+        self.points = self.points.map(add_x_coordinate)
+        return self
 
     def addYCoordinate(self):
         """adds the y coordinate of the geometry to the feature collection"""
@@ -63,15 +49,37 @@ class TrainingPoints(ee.FeatureCollection):
             geom = feature.geometry().centroid(1).coordinates().get(1)
             return feature.set("y", geom)
 
-        self._properties.append("y")
-        return self.map(add_y_coordinate)
+        self._add_propertity("y")
+        self.points = self.points.map(add_y_coordinate)
+        return self
 
-    def generateSamples(
-        self, image: ee.Image, scale: int = 10, tileScale: int = 16
-    ) -> ee.FeatureCollection:
+    def remap(self, lookup: ee.Dictionary):
+        """Remaps the classProperty of the feature collection using the lookup table"""
+
+        def remap(feature):
+            return feature.set(
+                self.classProperty, lookup.get(feature.get(self.classProperty))
+            )
+
+        self.points = self.points.map(remap)
+        return self
+
+    def addValues(self):
+        """adds the values of the lookup table to the feature collection"""
+
+        def add_values(feature):
+            return feature.set(
+                "value", self.lookup.get(feature.get(self.classProperty))
+            )
+
+        self._add_propertity("value")
+        self.points = self.points.map(add_values)
+        return self
+
+    def generateSamples(self, image: ee.Image, scale: int = 10, tileScale: int = 16):
         """Uses Image.sampleRegions method to generate training points from an image. Returns a geometryless feature collection with the specified properties."""
         sample = image.sampleRegions(
-            collection=self,
+            collection=self.points,
             scale=scale,
             properties=self.properties,
             tileScale=tileScale,
@@ -79,6 +87,7 @@ class TrainingPoints(ee.FeatureCollection):
         )
 
         pts = TrainingPoints(sample, self.classProperty)
+        pts.properties = self.properties
         return pts
 
     def getLookupTable(self) -> ee.FeatureCollection:
@@ -89,3 +98,7 @@ class TrainingPoints(ee.FeatureCollection):
             return ee.Feature(None, {"label": pair.get(0), "value": pair.get(1)})
 
         return ee.FeatureCollection(zipped.map(mkfeat))
+
+    def _add_propertity(self, value):
+        if value not in self.properties:
+            self.properties.append(value)
