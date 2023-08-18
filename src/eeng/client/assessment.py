a40040fc28e35ff5536a39789480bde9f71d4a57
Ryan Hamilton <ryangilberthamilton@gmail.com>
2023-08-17 21:49:04 -0400

    added confusion matrix from file func

--- src/eeng/client/assessment.py
+++ src/eeng/client/assessment.py
@@ -9,30 +9,34 @@ AssessmentTable = pd.DataFrame
 AssessmentFileName = str
 
 
-class AssessmentTable:
-    def __init__(self, filename: AssessmentFileName) -> None:
-        with open(filename, "r") as f:
-            data = json.load(f)
-        features = data['features']
-        props = [_.get("properties") for _ in features]
-        data = {k: v for _ in props for k, v in _.items()}
-        
-        self.data = data
+def confusion_matrix_from_file(datafile):
+    with open(datafile, "r") as f:
+        data = json.load(f)
 
-    def get_table(self) -> AssessmentTable:
-        # Confusion Matrix
-        cfm = pd.DataFrame(
-            data=self.data.get('cfm'),
-            columns=self.data.get('labels'),
-            index=self.data.get('labels')
-        )
-        cfm = cfm.reindex(columns=cfm.columns.tolist() + ['Producers'])
+    features = data["features"]
+    props = [_.get("properties") for _ in features]
+    data = {k: v for _ in props for k, v in _.items()}
 
-        new_index = pd.Index(cfm.index.tolist() + ['Consumers'])
-        cfm = cfm.reindex(new_index).fillna(value=np.nan)
-        pro = self.data.get('pro')
-        pro.append(np.nan)
-        cfm['Producers'] = pro
-        cfm.iloc[-1, 0:-1] = self.data.get('con')
+    # this construct the base table for the cond
+    cfm = pd.DataFrame(
+        data=data.get("cfm"),
+        columns=data.get("labels"),
+        index=data.get("labels"),
+    )
 
-        return cfm
+    # add producers to the base table
+    cfm = cfm.reindex(columns=cfm.columns.tolist() + ["Producers"])
+    pro = list(map(lambda x: round(x * 100, 2), data.get("pro")))
+
+    cfm["Producers"] = pro
+
+    # add consumers to the base table
+    new_index = pd.Index(cfm.index.tolist() + ["Consumers"])
+    cfm = cfm.reindex(new_index).fillna(value=np.nan)
+    cfm.iloc[-1, 0:-1] = list(map(lambda x: round(x * 100), data.get("con")))
+
+    # insert overall accuracy
+    cfm = cfm.reindex(cfm.index.tolist() + ["Overall Accuracy"]).fillna(value=np.nan)
+    cfm.iloc[-1, 0] = round(data.get("acc") * 100, 2)
+
+    return cfm
